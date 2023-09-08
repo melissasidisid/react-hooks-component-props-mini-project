f4c36250983c8c4c5cdf0c2e7a1594d93f22b2d4
melissasidisid <moimelly451@gmail.com>
2023-09-08 02:41:28 +0100

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
 import React from "react";
 import blogData from "../data/blog";
+import téléchargement from "./react-hooks-component-props-mini-project/téléchargement.jpg"
 
 console.log(blogData);
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <header>
+      <h1>Overeacted</h1>
+      </header>
+      <div className="about">
+      <aside>
+      
+      const [imgSrc, setImgSrc] = useState("Invalid Image Source");
+      <img src={téléchargement} alt="blog logo" onError = {() => setImgSrc("https://via.placeholder.com/215")}/>
+         <P>My Personal blog I explain codes and how they work</P>
+      </aside>
+
+         <div className="ArticleList">
+         <div className="article">
+          <h3>The wet code basse</h3>
+
+          <small>this is the latest article it takes<span>☕️3 minute </span></small>
+         </div>
+
+
+
+         </div>
+      </div>
     </div>
   );
 }
